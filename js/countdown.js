051eb14e5fba02db49e7872ea83245577f368d0c
sagarpise5 <somnathgaikawad511@gmail.com>
2015-12-03 12:47:40 +0530

    Change in countdown

--- js/countdown.js
+++ js/countdown.js
@@ -2,7 +2,10 @@ function fixIntegers(e) {
     return 0 > e && (e = 0), 10 > e ? "0" + e : "" + e
 }
 setInterval(function () {
-    var e = new Date("December 20 2015 11:56:00 GMT+0530"), t = new Date, r = Math.floor((e.getTime() - t.getTime()) / 1e3), n = fixIntegers(r % 60);
+    var e = new Date("December 20 2015 11:56:00 GMT+0530");
+    var t = new Date;
+    var r = Math.floor((e.getTime() - t.getTime()) / 1e3);
+    var n = fixIntegers(r % 60);
     r = Math.floor(r / 60);
     var a = fixIntegers(r % 60);
     r = Math.floor(r / 60);
